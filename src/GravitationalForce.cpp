ebf7c4d3b3600d38e92ada25184ffe06acecab61
Siyuan Huang <gz.siyuan@qq.com>
2024-11-09 01:18:23 +0100

    both types of force implemented

--- src/GravitationalForce.cpp
+++ src/GravitationalForce.cpp
@@ -8,8 +8,12 @@ GravitationalForce::GravitationalForce(double g): g(g) {}
 GravitationalForce::GravitationalForce(): g(1) {}
 
 std::array<double, 3> GravitationalForce::force(Particle particle1, Particle particle2) {
-    return scalar_mul(
-            this->g * particle1.getM() * particle2.getM() / pow(norm(sub(particle1.getX(), particle2.getX())), 3),
-            sub(particle1.getX(), particle2.getX())
-            );
+    std::array<double, 3> result{};
+    double r = distance(particle1.getX(), particle2.getX());
+    double factor = g * particle1.getM() * particle2.getM() / pow(r, 3);
+    for (int i = 0; i < 3; ++i) {
+        result[i] = factor * (particle1.getX()[i] - particle2.getX()[i]);
+    }
+    return result;
 }
+
--- src/LennardJonesForce.cpp
+++ src/LennardJonesForce.cpp
@@ -5,6 +5,12 @@ LennardJonesForce::LennardJonesForce(double epsilon, double sigma): epsilon(epsi
 LennardJonesForce::LennardJonesForce(): epsilon(5), sigma(1.1225) {}
 
 std::array<double, 3> LennardJonesForce::force(Particle particle1, Particle particle2) {
-    double distance = norm(sub(particle1.getX(), particle2.getX()));
-    return {0, 0, 0};
+    std::array<double, 3> result{};
+    double r = distance(particle1.getX(), particle2.getX());
+    double a = pow(sigma / r, 6);
+    double factor = 24 * epsilon / pow(r, 2) * (a - 2 * pow(a, 2));
+    for (int i = 0; i < 3; ++i) {
+        result[i] = factor * (particle1.getX()[i] - particle2.getX()[i]);
+    }
+    return result;
 }
