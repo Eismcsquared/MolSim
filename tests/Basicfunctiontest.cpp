3126d263162d5e734efcadf2dcf512d9dcd63d43
Weeniehut <ge48hal@tum.de>
2024-11-12 22:11:42 +0100

    make logfile on test.txt file

--- tests/Basicfunctiontest.cpp
+++ tests/Basicfunctiontest.cpp
@@ -7,45 +7,70 @@
 #include "FileReader.h"
 #include "utils/ArrayUtils.h"
 #include <string>
+#include <spdlog/spdlog.h>
+#include <spdlog/sinks/basic_file_sink.h>
 //#include "outputWriter/XYZWriter.h"
 //#include "outputWriter/VTKWriter.h"
 
 char* testfile = "../input/simpletest.txt";
+std::shared_ptr<spdlog::logger> test_logger = spdlog::basic_logger_mt("test_logger", "logs/test.txt");
 
-class PartilceTest : public ::testing::Test {
+class ParticleTest : public ::testing::Test {
 protected:
     std::vector<Particle> particles;
     ParticleContainer *pc;
 
     void SetUp() override {
+
         FileReader fileReader;
         fileReader.readFile(particles, testfile);
         GravitationalForce f;
         pc = new ParticleContainer(particles, 0, 1000, 0.014, f, ".vtu");
+        spdlog::set_level(spdlog::level::info);
+        test_logger -> info("Particle Container created");
+
     }
 
     void TearDown() override {
-        std::cout << "delete Particle Container " << "\n";
+        test_logger->info("delete Particle Container\n\n");
         delete pc;
     }
 };
 
-TEST_F(PartilceTest, Addparticle) {
+TEST_F(ParticleTest, Addparticle) {
+    test_logger->info("Add particle test1");
     Particle p = Particle({1,0,0}, {0,0,0}, 1, 0);
     int size_before = pc->getParticleSize();
     pc->addParticle(p);
     int size_after = pc->getParticleSize();
-    ASSERT_EQ(size_before + 1, size_after) ;
+    ASSERT_EQ(size_before + 1, size_after);
+    if(size_before + 1 == size_after) {
+        test_logger->info("Add particle test1 passed");
+    } else {
+        test_logger->error("Add particle test1 failed");
+    }
 }
 
-TEST_F(PartilceTest, Addparticle_2) {
-    std::cout << pc->getParticleSize() << "\n";
+TEST_F(ParticleTest, Addparticle_2) {
+    test_logger -> info("Add particle test2");
     int size_before = pc->getParticleSize();
-    for(int i = 0 ; i < 10; i++) {
-        Particle p = Particle({(double)i +2,0,0}, {0,0,0}, 1, 0);
+
+    bool flag = true;
+    for(int i = 1 ; i <= 10; i++) {
+        Particle p = Particle({(double)i +1,0,0}, {0,0,0}, 1, 0);
         pc->addParticle(p);
+        int size_after = pc->getParticleSize();
+        ASSERT_EQ(size_before + i, size_after) ;  
+        if(size_before + i == size_after) {
+        } else {
+            test_logger->error("Add particle test2 at iteration {} failed", i);
+            flag = false;
+            break;
+        }
     }
-    int size_after = pc->getParticleSize();
-    ASSERT_EQ(size_before + 10, size_after) ;
+    if(flag){
+        test_logger->info("Add particle test2 passed");
+    }
+
 }
 
