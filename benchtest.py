2bd66b7b397d9d9a07dee015366419620990e52c
Weeniehut <ge48hal@tum.de>
2024-11-14 23:42:02 +0100

    benchtest final

--- benchtest.py
+++ benchtest.py
@@ -37,7 +37,7 @@ def plot_benchmark(time_with_10_l, time_without_10_l, time_with_100_l, time_with
     plt.rcParams.update({'font.size': 10})
 
     plt.bar(labels, times, color=['blue', 'red', 'blue', 'red', 'blue', 'red'])
-    plt.title('Average Execution Time for Molsim.cpp with and without Newton\'s 3rd law(LennardJonesForce)', fontsize=8)
+    plt.title('Average Execution Time for Molsim.cpp with and without Newton\'s 3rd law(GravitationalForce)', fontsize=8)
     plt.ylabel('Average Execution Time (seconds)', fontsize=10)
     plt.xticks(rotation=45, ha='right', fontsize=9)
     plt.yticks(fontsize=9)
@@ -49,31 +49,30 @@ def main():
     mol_sim_path = "./build/MolSim"
     input_file = "./input/eingabe-sonne.txt"
 
-    # Run benchmark with Newton's 3rd law + -l flag for different iterations
-    avg_time_with_10_l = run_benchmark(mol_sim_path, input_file, benchmark_option="-b", newton_option="", flag_option="-l", num_iterations=10)
-    if avg_time_with_10_l is None:
+    avg_time_with_10 = run_benchmark(mol_sim_path, input_file, benchmark_option="-b", newton_option="", flag_option="-g", num_iterations=10)
+    if avg_time_with_10 is None:
         return
-    avg_time_without_10_l = run_benchmark(mol_sim_path, input_file, benchmark_option="-b", newton_option="-n", flag_option="-l", num_iterations=10)
-    if avg_time_without_10_l is None:
-        return
-
-    avg_time_with_100_l = run_benchmark(mol_sim_path, input_file, benchmark_option="-b", newton_option="", flag_option="-l", num_iterations=100)
-    if avg_time_with_100_l is None:
-        return
-    avg_time_without_100_l = run_benchmark(mol_sim_path, input_file, benchmark_option="-b", newton_option="-n", flag_option="-l", num_iterations=100)
-    if avg_time_without_100_l is None:
+    avg_time_without_10 = run_benchmark(mol_sim_path, input_file, benchmark_option="-b", newton_option="-n", flag_option="-g", num_iterations=10)
+    if avg_time_without_10 is None:
         return
 
-    avg_time_with_1000_l = run_benchmark(mol_sim_path, input_file, benchmark_option="-b", newton_option="", flag_option="-l", num_iterations=1000)
-    if avg_time_with_1000_l is None:
+    avg_time_with_100 = run_benchmark(mol_sim_path, input_file, benchmark_option="-b", newton_option="", flag_option="-g", num_iterations=100)
+    if avg_time_with_100 is None:
         return
-    avg_time_without_1000_l = run_benchmark(mol_sim_path, input_file, benchmark_option="-b", newton_option="-n", flag_option="-l", num_iterations=1000)
-    if avg_time_without_1000_l is None:
+    avg_time_without_100 = run_benchmark(mol_sim_path, input_file, benchmark_option="-b", newton_option="-n", flag_option="-g", num_iterations=100)
+    if avg_time_without_100 is None:
+        return
+
+    avg_time_with_1000 = run_benchmark(mol_sim_path, input_file, benchmark_option="-b", newton_option="", flag_option="-g", num_iterations=1000)
+    if avg_time_with_1000 is None:
+        return
+    avg_time_without_1000 = run_benchmark(mol_sim_path, input_file, benchmark_option="-b", newton_option="-n", flag_option="-g", num_iterations=1000)
+    if avg_time_without_1000 is None:
         return
 
     # Plot average execution times for comparison across 10, 100, and 1000 iterations with -l flag
     plot_benchmark(
-        avg_time_with_10_l, avg_time_without_10_l, avg_time_with_100_l, avg_time_without_100_l, avg_time_with_1000_l, avg_time_without_1000_l
+        avg_time_with_10, avg_time_without_10, avg_time_with_100, avg_time_without_100, avg_time_with_1000, avg_time_without_1000
     )
 
 if __name__ == "__main__":
