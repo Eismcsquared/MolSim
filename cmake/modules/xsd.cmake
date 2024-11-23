39c25e555bef0ce8cb876b6d2094002f8c2e4891
Siyuan Huang <ge95fij@tum.de>
2024-11-23 22:38:02 +0100

    defined xml input schema

--- cmake/modules/xsd.cmake
+++ cmake/modules/xsd.cmake
@@ -1,12 +1,13 @@
-find_program(XSD_EXECUTABLE NAMES xsdcxx)
+find_program(XSD_EXECUTABLE xsdcxx)
 
 if (XSD_EXECUTABLE)
     file(GLOB_RECURSE XSD
-            "${CMAKE_CURRENT_SOURCE_DIR}/src/inputReader/model/*.xsd"
+            "${CMAKE_CURRENT_SOURCE_DIR}/src/inputReader/*.xsd"
     )
     add_custom_target(xsd
-            COMMAND ${XSD_EXECUTABLE} cxx-tree --hxx-suffix=.h --cxx-suffix=.cpp --std c++20 --generate-doxygen --generate-serialization ${XSD}
-            WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}/src/inputReader/model
+            COMMAND ${XSD_EXECUTABLE} cxx-tree --hxx-suffix .h --cxx-suffix .cpp --generate-doxygen --generate-serialization ${XSD}
+            WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}/src/inputReader
+            COMMENT "Generating code from xml schemas"
             VERBATIM
     )
 endif()
