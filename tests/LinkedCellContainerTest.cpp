bd2f1e7c4ae5d40f3c4bb59bc6d86d27ff3087da
Siyuan Huang <ge95fij@tum.de>
2024-12-09 00:00:45 +0100

    added tests for periodic boundary condition

--- tests/LinkedCellContainerTest.cpp
+++ tests/LinkedCellContainerTest.cpp
@@ -9,16 +9,26 @@ class LinkedCellContainerTest: public ::testing::Test {
 protected:
     std::unique_ptr<LinkedCellContainer> container2D;
     std::unique_ptr<LinkedCellContainer> container3D;
+    std::unique_ptr<LinkedCellContainer> container3DPeriodicX;
+    std::unique_ptr<LinkedCellContainer> container3DPeriodicAll;
 
     void SetUp() override {
         std::unique_ptr<std::vector<Particle>> p1 = std::make_unique<std::vector<Particle>>();
         std::unique_ptr<std::vector<Particle>> p2 = std::make_unique<std::vector<Particle>>();
+        std::unique_ptr<std::vector<Particle>> p3 = std::make_unique<std::vector<Particle>>();
+        std::unique_ptr<std::vector<Particle>> p4 = std::make_unique<std::vector<Particle>>();
         std::unique_ptr<Force> f1 = std::make_unique<LennardJonesForce>();
         std::unique_ptr<Force> f2 = std::make_unique<LennardJonesForce>();
+        std::unique_ptr<Force> f3 = std::make_unique<LennardJonesForce>();
+        std::unique_ptr<Force> f4 = std::make_unique<LennardJonesForce>();
         container2D = std::make_unique<LinkedCellContainer>(p1, f1, std::array<double, 3>{15, 15, 1}, 3,
                                           std::array<BoundaryCondition, 6>{OUTFLOW, OUTFLOW, OUTFLOW, OUTFLOW, OUTFLOW, OUTFLOW});
         container3D = std::make_unique<LinkedCellContainer>(p2, f2, std::array<double, 3>{100, 100, 50}, 3,
                                           std::array<BoundaryCondition, 6>{REFLECTING, REFLECTING, REFLECTING, REFLECTING, OUTFLOW, OUTFLOW});
+        container3DPeriodicX = std::make_unique<LinkedCellContainer>(p3, f3, std::array<double, 3>{100, 100, 50}, 3,
+                                                                     std::array<BoundaryCondition, 6>{PERIODIC, PERIODIC, REFLECTING, REFLECTING, OUTFLOW, OUTFLOW});
+        container3DPeriodicAll = std::make_unique<LinkedCellContainer>(p4, f4, std::array<double, 3>{100, 100, 50}, 3,
+                                                                       std::array<BoundaryCondition, 6>{PERIODIC, PERIODIC, PERIODIC, PERIODIC, PERIODIC, PERIODIC});
         test_logger -> info("LinkedCellContainer created");
     }
 
@@ -33,7 +43,7 @@ TEST_F(LinkedCellContainerTest, CellInstantiation) {
     EXPECT_EQ(5, container2D->getNCells()[0]);
     EXPECT_EQ(5, container2D->getNCells()[1]);
     EXPECT_EQ(1, container2D->getNCells()[2]);
-    for (auto c: container2D->getCells()) {
+    for (const auto& c: container2D->getCells()) {
         EXPECT_EQ(3, c.getSize()[0]);
         EXPECT_EQ(3, c.getSize()[1]);
         EXPECT_EQ(1, c.getSize()[2]);
@@ -74,6 +84,7 @@ TEST_F(LinkedCellContainerTest, Indices) {
     }
 }
 
+// Test whether neighbouring cells are computed correctly.
 TEST_F(LinkedCellContainerTest, Neighbours) {
     test_logger->info("LinkedCellContainer - Neighbour indices test");
     // inner cell
@@ -81,16 +92,22 @@ TEST_F(LinkedCellContainerTest, Neighbours) {
                           12862, 12861, 12863, 12896, 12897, 12898, 12826, 12827, 12828,
                           10412, 10411, 10413, 10446, 10447, 10448, 10376, 10377, 10378};
     EXPECT_EQ(ref1, container3D->getCells()[11637].getNeighbours());
+    EXPECT_EQ(ref1, container3DPeriodicX->getCells()[11637].getNeighbours());
     // cell at boundary
     std::set<int> ref2 = {1523, 1525, 1558, 1559, 1560, 1488, 1489, 1490,
                           2749, 2748, 2750, 2783, 2784, 2785, 2713, 2714, 2715};
     EXPECT_EQ(ref2, container3D->getCells()[1524].getNeighbours());
+    EXPECT_EQ(ref2, container3DPeriodicX->getCells()[1524].getNeighbours());
     // cell at vertex
     std::set<int> ref3 = {7387, 7421, 7422, 8611, 8612, 8646, 8647, 6161, 6162, 6196, 6197};
+    std::set<int> ref3Periodic = {7387, 7418, 7421, 7422, 7453, 8611, 8612, 8643, 8646, 8647, 8678, 6161, 6162, 6193, 6196, 6197, 6228};
     EXPECT_EQ(ref3, container3D->getCells()[7386].getNeighbours());
+    EXPECT_EQ(ref3Periodic, container3DPeriodicX->getCells()[7386].getNeighbours());
     // cell at corner
     std::set<int> ref4 = {20787, 20753, 20752, 19563, 19562, 19528, 19527};
+    std::set<int> ref4Periodic = {20787, 20756, 20753, 20752, 20721, 19563, 19562, 19531, 19528, 19527, 19496};
     EXPECT_EQ(ref4, container3D->getCells()[20788].getNeighbours());
+    EXPECT_EQ(ref4Periodic, container3DPeriodicX->getCells()[20788].getNeighbours());
     // halo cells
     std::set<int> empty{};
     EXPECT_EQ(empty, container3D->getCells()[0].getNeighbours());
@@ -99,6 +116,17 @@ TEST_F(LinkedCellContainerTest, Neighbours) {
     EXPECT_EQ(empty, container3D->getCells()[12244].getNeighbours());
     EXPECT_EQ(empty, container3D->getCells()[5740].getNeighbours());
     EXPECT_EQ(empty, container3D->getCells()[5424].getNeighbours());
+    EXPECT_EQ(empty, container3DPeriodicX->getCells()[0].getNeighbours());
+    EXPECT_EQ(empty, container3DPeriodicX->getCells()[337].getNeighbours());
+    EXPECT_EQ(empty, container3DPeriodicX->getCells()[1237].getNeighbours());
+    EXPECT_EQ(empty, container3DPeriodicX->getCells()[12244].getNeighbours());
+    EXPECT_EQ(empty, container3DPeriodicX->getCells()[5740].getNeighbours());
+    EXPECT_EQ(empty, container3DPeriodicX->getCells()[5424].getNeighbours());
+    if (::testing::Test::HasFailure()) {
+        test_logger->info("LinkedCellContainer - Neighbour indices test failed");
+    } else {
+        test_logger->info("LinkedCellContainer - Neighbour indices test passed");
+    }
 }
 
 // Test whether the method removeFrom works correctly
@@ -145,7 +173,7 @@ TEST_F(LinkedCellContainerTest, ForceCalculation) {
     std::array<std::array<double, 3>, 8> pos = {
             std::array<double, 3>{10, 10, 10},
             std::array<double, 3>{11, 11, 11},
-            std::array<double, 3>{13, 9, 9} // "for away" form the first two
+            std::array<double, 3>{13, 9, 9} // "far away" form the first two
     };
 
     // updateF called in addParticle, test the calculation
@@ -170,9 +198,91 @@ TEST_F(LinkedCellContainerTest, ForceCalculation) {
     }
 }
 
+// Test the correctness of the force calculation in case of periodic boundary condition.
+TEST_F(LinkedCellContainerTest, ForceCalculationPeriodic) {
+    test_logger->info("LinkedCellContainer - Periodic force calculation test");
+    std::array<std::array<double, 3>, 8> pos = {
+            std::array<double, 3>{1, 2, 2},
+            std::array<double, 3>{99, 2, 2},
+            std::array<double, 3>{99, 2, 4},
+            std::array<double, 3>{99, 4, 2},
+            std::array<double, 3>{1, 99, 2}, // no interaction with the first particle since the y-boundaries are not periodic
+            std::array<double, 3>{1, 2, 49}, // no interaction with the first particle since the z-boundaries are not periodic
+    };
+
+    auto force = [](double r) {
+        return 120 * (pow(r, -7) - 2 * pow(r, -13));
+    };
+
+    container3DPeriodicX->addParticle(Particle(pos[0], {0, 0, 0}, 1));
+    container3DPeriodicX->addParticle(Particle(pos[1], {0, 0, 0}, 1));
+
+    EXPECT_NEAR(-force(2), container3DPeriodicX->getParticles()[0].getF()[0], 1e-12);
+    EXPECT_NEAR(0, container3DPeriodicX->getParticles()[0].getF()[1], 1e-12);
+    EXPECT_NEAR(0, container3DPeriodicX->getParticles()[0].getF()[2], 1e-12);
+    EXPECT_NEAR(force(2), container3DPeriodicX->getParticles()[1].getF()[0], 1e-12);
+    EXPECT_NEAR(0, container3DPeriodicX->getParticles()[1].getF()[1], 1e-12);
+    EXPECT_NEAR(0, container3DPeriodicX->getParticles()[1].getF()[2], 1e-12);
+
+    container3DPeriodicX->addParticle(Particle(pos[2], {0, 0, 0}, 1));
+
+    EXPECT_NEAR(-force(2) - force(sqrt(8)) / sqrt(2), container3DPeriodicX->getParticles()[0].getF()[0], 1e-12);
+    EXPECT_NEAR(0, container3DPeriodicX->getParticles()[0].getF()[1], 1e-12);
+    EXPECT_NEAR(force(sqrt(8)) / sqrt(2), container3DPeriodicX->getParticles()[0].getF()[2], 1e-12);
+    EXPECT_NEAR(force(2), container3DPeriodicX->getParticles()[1].getF()[0], 1e-12);
+    EXPECT_NEAR(0, container3DPeriodicX->getParticles()[1].getF()[1], 1e-12);
+    EXPECT_NEAR(force(2), container3DPeriodicX->getParticles()[1].getF()[2], 1e-12);
+    EXPECT_NEAR(force(sqrt(8)) / sqrt(2), container3DPeriodicX->getParticles()[2].getF()[0], 1e-12);
+    EXPECT_NEAR(0, container3DPeriodicX->getParticles()[2].getF()[1], 1e-12);
+    EXPECT_NEAR(-force(2) - force(sqrt(8)) / sqrt(2), container3DPeriodicX->getParticles()[2].getF()[2], 1e-12);
+
+    container3DPeriodicX->addParticle(Particle(pos[3], {0, 0, 0}, 1));
+
+    EXPECT_NEAR(-force(2) - 2 * force(sqrt(8)) / sqrt(2), container3DPeriodicX->getParticles()[0].getF()[0], 1e-12);
+    EXPECT_NEAR(force(sqrt(8)) / sqrt(2), container3DPeriodicX->getParticles()[0].getF()[1], 1e-12);
+    EXPECT_NEAR(force(sqrt(8)) / sqrt(2), container3DPeriodicX->getParticles()[0].getF()[2], 1e-12);
+    EXPECT_NEAR(force(2), container3DPeriodicX->getParticles()[1].getF()[0], 1e-12);
+    EXPECT_NEAR(force(2), container3DPeriodicX->getParticles()[1].getF()[1], 1e-12);
+    EXPECT_NEAR(force(2), container3DPeriodicX->getParticles()[1].getF()[2], 1e-12);
+    EXPECT_NEAR(force(sqrt(8)) / sqrt(2), container3DPeriodicX->getParticles()[2].getF()[0], 1e-12);
+    EXPECT_NEAR(force(sqrt(8)) / sqrt(2), container3DPeriodicX->getParticles()[2].getF()[1], 1e-12);
+    EXPECT_NEAR(-force(2) - 2 * force(sqrt(8)) / sqrt(2), container3DPeriodicX->getParticles()[2].getF()[2], 1e-12);
+    EXPECT_NEAR(force(sqrt(8)) / sqrt(2), container3DPeriodicX->getParticles()[3].getF()[0], 1e-12);
+    EXPECT_NEAR(-force(2) - 2 * force(sqrt(8)) / sqrt(2), container3DPeriodicX->getParticles()[3].getF()[1], 1e-12);
+    EXPECT_NEAR(force(sqrt(8)) / sqrt(2), container3DPeriodicX->getParticles()[3].getF()[2], 1e-12);
+
+    container3DPeriodicX->addParticle(Particle(pos[4], {0, 0, 0}, 1));
+    container3DPeriodicX->addParticle(Particle(pos[5], {0, 0, 0}, 1));
+
+    EXPECT_NEAR(-force(2) - 2 * force(sqrt(8)) / sqrt(2), container3DPeriodicX->getParticles()[0].getF()[0], 1e-12);
+    EXPECT_NEAR(force(sqrt(8)) / sqrt(2), container3DPeriodicX->getParticles()[0].getF()[1], 1e-12);
+    EXPECT_NEAR(force(sqrt(8)) / sqrt(2), container3DPeriodicX->getParticles()[0].getF()[2], 1e-12);
+    EXPECT_NEAR(force(2), container3DPeriodicX->getParticles()[1].getF()[0], 1e-12);
+    EXPECT_NEAR(force(2), container3DPeriodicX->getParticles()[1].getF()[1], 1e-12);
+    EXPECT_NEAR(force(2), container3DPeriodicX->getParticles()[1].getF()[2], 1e-12);
+    EXPECT_NEAR(force(sqrt(8)) / sqrt(2), container3DPeriodicX->getParticles()[2].getF()[0], 1e-12);
+    EXPECT_NEAR(force(sqrt(8)) / sqrt(2), container3DPeriodicX->getParticles()[2].getF()[1], 1e-12);
+    EXPECT_NEAR(-force(2) - 2 * force(sqrt(8)) / sqrt(2), container3DPeriodicX->getParticles()[2].getF()[2], 1e-12);
+    EXPECT_NEAR(force(sqrt(8)) / sqrt(2), container3DPeriodicX->getParticles()[3].getF()[0], 1e-12);
+    EXPECT_NEAR(-force(2) - 2 * force(sqrt(8)) / sqrt(2), container3DPeriodicX->getParticles()[3].getF()[1], 1e-12);
+    EXPECT_NEAR(force(sqrt(8)) / sqrt(2), container3DPeriodicX->getParticles()[3].getF()[2], 1e-12);
+    EXPECT_NEAR(0, container3DPeriodicX->getParticles()[4].getF()[0], 1e-12);
+    EXPECT_NEAR(0, container3DPeriodicX->getParticles()[4].getF()[1], 1e-12);
+    EXPECT_NEAR(0, container3DPeriodicX->getParticles()[4].getF()[2], 1e-12);
+    EXPECT_NEAR(0, container3DPeriodicX->getParticles()[5].getF()[0], 1e-12);
+    EXPECT_NEAR(0, container3DPeriodicX->getParticles()[5].getF()[1], 1e-12);
+    EXPECT_NEAR(0, container3DPeriodicX->getParticles()[5].getF()[2], 1e-12);
+
+    if (::testing::Test::HasFailure()) {
+        test_logger->info("LinkedCellContainer - Periodic force calculation test failed");
+    } else {
+        test_logger->info("LinkedCellContainer - Periodic force calculation test passed");
+    }
+}
+
 // Test the outflow boundary condition
 TEST_F(LinkedCellContainerTest, Outflow) {
-    test_logger->info("LinkedCellContainer - Outflow test");
+    test_logger->info("LinkedCellContainer - Outflow boundary test");
     container2D->addParticle(Particle({1, 1, 0.5}, {-1, 0, 0}, 1)); // outflow at t=1
     container2D->addParticle(Particle({13, 13, 0.5}, {0, 1, 0}, 1)); // outflow at t=2
     container2D->addParticle(Particle({13, 1, 0.5}, {sqrt(2), sqrt(2), 0}, 1)); // outflow at t=sqrt(2)
@@ -190,35 +300,66 @@ TEST_F(LinkedCellContainerTest, Outflow) {
     container2D->simulate(0.02, 1e-3, "", "", 10, false, true);
     EXPECT_EQ(0, container2D->getParticleNumber());
     if (::testing::Test::HasFailure()) {
-        test_logger->info("LinkedCellContainer - Outflow test failed");
+        test_logger->info("LinkedCellContainer - Outflow boundary test failed");
     } else {
-        test_logger->info("LinkedCellContainer - Outflow test passed");
+        test_logger->info("LinkedCellContainer - Outflow boundary test passed");
     }
 }
 
+// Test the reflecting boundary condition
 TEST_F(LinkedCellContainerTest, Reflecting) {
-    test_logger->info("LinkedCellContainer - Reflecting test");
+    test_logger->info("LinkedCellContainer - Reflecting boundary test");
     container3D->addParticle(Particle({1, 1, 10}, {-1, 0, 0}, 1)); //reflected at t=1
     container3D->addParticle(Particle({98, 98, 10}, {1, 1, 0}, 1)); //reflected at t=2
     container3D->addParticle(Particle({50, 1, 48}, {2, -1, 1}, 1)); //reflect at t=1, outflow at t=2
-    container3D->simulate(1.5, 1e-2, "", "", 10, false, true);
-    EXPECT_NEAR(0.5, container3D->getParticles()[0].getX()[0], 2e-2);
-    EXPECT_FLOAT_EQ(1, container3D->getParticles()[0].getV()[0]);
-    EXPECT_NEAR(53, container3D->getParticles()[2].getX()[0], 2e-2);
-    EXPECT_NEAR(0.5, container3D->getParticles()[2].getX()[1], 2e-2);
-    EXPECT_NEAR(49.5, container3D->getParticles()[2].getX()[2], 2e-2);
-    EXPECT_FLOAT_EQ(1, container3D->getParticles()[2].getV()[1]);
+    container3D->simulate(1.5, 1e-1, "", "", 10, false, true);
+
     EXPECT_EQ(3, container3D->getParticleNumber());
-    container3D->simulate(1, 1e-2, "", "", 10, false, true);
-    EXPECT_NEAR(99.5, container3D->getParticles()[1].getX()[0], 2e-2);
-    EXPECT_NEAR(99.5, container3D->getParticles()[1].getX()[1], 2e-2);
-    EXPECT_FLOAT_EQ(-1, container3D->getParticles()[1].getV()[0]);
-    EXPECT_FLOAT_EQ(-1, container3D->getParticles()[1].getV()[1]);
+    EXPECT_EQ(Particle({0.5, 1, 10}, {1, 0, 0}, 1), container3D->getParticles()[0]);
+    EXPECT_EQ(Particle({99.5, 99.5, 10}, {1, 1, 0}, 1), container3D->getParticles()[1]);
+    EXPECT_EQ(Particle({53, 0.5, 49.5}, {2, 1, 1}, 1), container3D->getParticles()[2]);
+
+    container3D->simulate(1, 1e-1, "", "", 10, false, true);
+
     EXPECT_EQ(2, container3D->getParticleNumber());
+    EXPECT_FALSE(container3D->getParticles()[2].isInDomain());
+    EXPECT_EQ(Particle({1.5, 1, 10}, {1, 0, 0}, 1), container3D->getParticles()[0]);
+    EXPECT_EQ(Particle({99.5, 99.5, 10}, {-1, -1, 0}, 1), container3D->getParticles()[1]);
+
     if (::testing::Test::HasFailure()) {
-        test_logger->info("LinkedCellContainer - Reflecting test failed");
+        test_logger->info("LinkedCellContainer - Reflecting boundary test failed");
     } else {
-        test_logger->info("LinkedCellContainer - Reflecting test passed");
+        test_logger->info("LinkedCellContainer - Reflecting boundary test passed");
+    }
+}
+
+// Test the periodic boundary condition
+TEST_F(LinkedCellContainerTest, Periodic) {
+    test_logger->info("LinkedCellContainer - Periodic boundary test");
+    container3DPeriodicX->addParticle(Particle({1, 1, 10}, {-1, 0, 0}, 1)); // periodic at t=1
+    container3DPeriodicX->addParticle(Particle({98, 98, 10}, {1, 1, 0}, 1)); // periodic + reflecting at t=2
+    container3DPeriodicX->addParticle(Particle({1, 1, 48}, {-1, 0, 2}, 1)); // periodic + outflow at t=1
+
+    container3DPeriodicX->simulate(1.5, 1e-1, "", "", 10, false, true);
+    EXPECT_EQ(2, container3DPeriodicX->getParticleNumber());
+    EXPECT_FALSE(container3DPeriodicX->getParticles()[2].isInDomain());
+    EXPECT_EQ(Particle({99.5, 1, 10}, {-1, 0, 0}, 1), container3DPeriodicX->getParticles()[0]);
+    EXPECT_EQ(Particle({99.5, 99.5, 10}, {1, 1, 0}, 1), container3DPeriodicX->getParticles()[1]);
+    container3DPeriodicX->simulate(1, 1e-1, "", "", 10, false, true);
+    EXPECT_EQ(2, container3DPeriodicX->getParticleNumber());
+    EXPECT_EQ(Particle({98.5, 1, 10}, {-1, 0, 0}, 1), container3DPeriodicX->getParticles()[0]);
+    EXPECT_EQ(Particle({0.5, 99.5, 10}, {1, -1, 0}, 1), container3DPeriodicX->getParticles()[1]);
+
+    // What happens at "periodic corners"?
+
+    container3DPeriodicAll->addParticle(Particle({1, 1, 1}, {-1, -1, -1}, 1));
+    container3DPeriodicAll->simulate(2, 1e-1, "", "", 10, false, true);
+    EXPECT_EQ(Particle({99, 99, 49}, {-1, -1, -1}, 1), container3DPeriodicAll->getParticles()[0]);
+
+    if (::testing::Test::HasFailure()) {
+        test_logger->info("LinkedCellContainer - Periodic boundary test failed");
+    } else {
+        test_logger->info("LinkedCellContainer - Periodic boundary test passed");
     }
 }
 
