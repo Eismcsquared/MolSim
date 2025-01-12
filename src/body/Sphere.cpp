c79e72ebbba4e9f067a1f47d0a37ec0da5f89b3d
Siyuan Huang <ge95fij@tum.de>
2025-01-12 18:23:24 +0100

    removed redundant codes in Sphere

--- src/body/Sphere.cpp
+++ src/body/Sphere.cpp
@@ -13,17 +13,11 @@ Sphere::Sphere(const std::array<double, 3> &x, const std::array<double, 3> &v, i
 void Sphere::createParticles(std::vector<Particle> &particles) const {
     for (int i = -radius; i <= radius; ++i) {
         for (int j = -radius; j <= radius; ++j) {
-            if (dimension == 2) {
-                std::array<double, 3> pos = x + std::array<double, 3> {i * distance, j * distance, 0};
-                if(ArrayUtils::L2NormSquare(x - pos) <= pow(radius * distance, 2)) {
+            int r = dimension == 2 ? 0 : radius;
+            for (int k = -r; k <= r; ++k) {
+                std::array<double, 3> pos = x + std::array<double, 3> {i * distance, j * distance, k * distance};
+                if(ArrayUtils::L2NormSquare(x - pos) <= radius * radius * distance * distance) {
                     particles.emplace_back(pos, v + maxwellBoltzmannDistributedVelocity(avgVelocityBrownian, dimension), m, type, epsilon, sigma);
-                }
-            } else {
-                for (int k = -radius; k <= radius; ++k) {
-                    std::array<double, 3> pos = x + std::array<double, 3> {i * distance, j * distance, k * distance};
-                    if(ArrayUtils::L2NormSquare(x - pos) <= radius * radius * distance * distance) {
-                        particles.emplace_back(pos, v + maxwellBoltzmannDistributedVelocity(avgVelocityBrownian, dimension), m, type, epsilon, sigma);
-                    }
                 }
             }
         }
